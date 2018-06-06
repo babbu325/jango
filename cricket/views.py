b16fda77d888f6a427044c27206c6792a648df4f
ramendra <babbus325@gmail.com>
2018-06-06 06:17:52 +0530

    django tutorial

--- cricket/views.py
+++ cricket/views.py
@@ -1,15 +1,17 @@
-from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render
 from .models import Team
 
 
 def index(request):
     all_teams = Team.objects.all()
-    html = ''
-    for team in all_teams:
-        url = '/cricket/' + str(team.id) + '/'
-        html += '<a href = "' + url + '">' + team.Team_name + '</a> <br>'
-    return HttpResponse(html)
+    contaxt = {'all_teams': all_teams}
+    return render(request, 'cricket/index.html', contaxt)
 
 
 def detail(request, team_id):
-    return HttpResponse("<h1>Details fro the team id: " + str(team_id) + " </h1>")
+    try:
+        team = Team.objects.get(pk=team_id)
+    except Team.DoesNotExist:
+        raise Http404("No team with this id...")
+    return render(request, 'cricket/detail.html', {'team_id': team})
